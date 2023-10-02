4ebaafd9c3fdb7597760d1504cbfc97b8ed8f8ad
Alexander Mitev <a.mitev91@gmail.com>
2023-10-02 15:19:24 +0300

    Updated ACL policies

--- main.tf
+++ main.tf
@@ -4,6 +4,22 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "bucket" {
   bucket = aws_s3_bucket.bucket.id
 
@@ -17,12 +33,21 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
 }
 
 resource "aws_s3_bucket_acl" "bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket,
+    aws_s3_bucket_public_access_block.bucket,
+  ]
   bucket = aws_s3_bucket.bucket.id
 
   acl = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "policy" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.web,
+    aws_s3_bucket_public_access_block.web,
+  ]
+
   bucket = aws_s3_bucket.bucket.id
   policy = <<EOF
 {
