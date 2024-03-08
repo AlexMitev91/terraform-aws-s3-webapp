a13350e46c3bb7e226a9cfa3b9c9173bbc3c965d
Alexander Mitev <a.mitev91@gmail.com>
2024-03-08 12:57:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,10 @@
-# TESTING
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
 
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = "${var.prefix}-${var.name}"
 
   force_destroy = true
-}
-
-resource "aws_s3_bucket_ownership_controls" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
 }
 
 resource "aws_s3_bucket_public_access_block" "bucket" {
@@ -20,6 +14,13 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_website_configuration" "bucket" {
@@ -36,8 +37,8 @@ resource "aws_s3_bucket_website_configuration" "bucket" {
 
 resource "aws_s3_bucket_acl" "bucket" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.bucket,
     aws_s3_bucket_public_access_block.bucket,
+    aws_s3_bucket_ownership_controls.bucket,
   ]
   bucket = aws_s3_bucket.bucket.id
 
@@ -45,11 +46,6 @@ resource "aws_s3_bucket_acl" "bucket" {
 }
 
 resource "aws_s3_bucket_policy" "policy" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.bucket,
-    aws_s3_bucket_public_access_block.bucket,
-  ]
-
   bucket = aws_s3_bucket.bucket.id
   policy = <<EOF
 {
@@ -72,12 +68,6 @@ EOF
 }
 
 resource "aws_s3_object" "webapp" {
-  depends_on = [
-    aws_s3_bucket_acl.bucket,
-    aws_s3_bucket_ownership_controls.bucket,
-    aws_s3_bucket_public_access_block.bucket,
-  ]
-
   acl          = "public-read"
   key          = "index.html"
   bucket       = aws_s3_bucket.bucket.id
